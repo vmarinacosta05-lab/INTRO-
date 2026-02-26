f49ad7a84b226f5d280abad7af9d480e8a5c4918
vmarinacosta05-lab <vmarinacosta05@gmail.com>
2026-02-26 13:21:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title ("DO YOU EVER FEEL LIKE A PLASTIC BAG")
 st.header("la mejor cancion de Katty Perry")
+from PIL import Image
+image=Image.open("katty.jpg")
+st.image(image,caption="WOOOOOO")
